3a4bc04b1a5bc5a2c4e1b87a004a5eabd3edaffc
shawntai <shawn890715@gmail.com>
2022-10-08 15:28:42 -0400

    fix: dropdown-select height, add more options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
+import { countries } from "countries-list";
 import React from "react";
 import Dropdown from "./dropdown/Dropdown";
 
 const App = () => {
-  const options = [
-    {value: "argentina", label: "Argentina"},
-    {value: "taiwan", label: "Taiwan"},
-    {value: "argentdina", label: "Aasargentina"},
-    {value: "taiwdsan", label: "Taiwasasasn"},
-    {value: "argedsntina", label: "Argensdasasatina"},
-    {value: "taxsxiwan", label: "Taiwzczccan"},
-    {value: "argexsxsntina", label: "Argecxcxntina"},
-    {value: "taiwccsan", label: "Taiwdsxsdsxxan"},
-    {value: "argenerfetina", label: "Argentxsxsxina"},
-    {value: "taiwrean", label: "Taicsxzcwan"},
-  ];
-  return (
-    <>
-      <Dropdown options={options} mode="multiple" />
-      <Dropdown options={options} mode="single" />
-    </>
-  )
-}
+	const options = Object.keys(countries).map((code) => {
+		return {
+			value: countries[code].name,
+			label: countries[code].name,
+		};
+	});
+	return (
+		<>
+			<Dropdown options={options} mode="multiple" />
+			<Dropdown options={options} mode="single" />
+		</>
+	);
+};
 
 export default App;
